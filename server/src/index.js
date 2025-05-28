1b288f57e6c65117cccf5b608b07fa7a19592cbf
Mahib09 <magarmahib@gmail.com>
2025-05-28 19:19:10 -0400

    Fix: use dynamic PORT and update CORS origin for deployment

--- server/src/index.js
+++ server/src/index.js
@@ -15,7 +15,7 @@ app.options("*", cors());
 
 setupRedis().then((client) => {
   app.locals.redis = client;
-  const PORT = 3001;
+  const PORT = process.env.PORT || 3001;
   app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
   });
