0095b29adecce9ddcaa997dd6951aaddb04897d7
Mahib09 <magarmahib@gmail.com>
2025-05-28 17:59:50 -0400

    dev: Add start script for Heroku

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -36,7 +36,7 @@ const HeadBar = () => {
   };
   return (
     <header className="border border-dashed border-x-0 border-t-0 w-full">
-      <div className="md:mx-12 xl:mx-20 mx-0 border border-dashed border-y-0">
+      <div className="md:mx-10 xl:mx-10 mx-0 border border-dashed border-y-0">
         <div className="flex gap-2 px-4 py-2 items-center ">
           {open && !isMobile ? <></> : <SidebarTrigger />}
 
